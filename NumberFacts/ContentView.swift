a1f6e07b6fd97a2955d3d6801b829f91e4ae2e3c
Isla Russell-Howes <irussellhowes@lcs.on.ca>
2025-01-20 09:45:48 -0500

    Initial Commit

---
+++ NumberFacts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NumberFacts
+//
+//  Created by Isla Russell-Howes on 2025-01-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NumberFacts/NumberFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberFactsApp.swift
+//  NumberFacts
+//
+//  Created by Isla Russell-Howes on 2025-01-20.
+//
+
+import SwiftUI
+
+@main
+struct NumberFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
